e2e5dac8649a3687c6466c0df9aff7bf05b10409
Rene Sandoval <renefs127@gmail.com>
2026-03-06 02:30:45 -0700

    conexion typeorm

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AlumnosModule } from './alumnos/alumnos.module';
 
 @Module({
-  imports: [AlumnosModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: typeOrmConfig,
+      inject: [ConfigService],
+    }),
+    AlumnosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
